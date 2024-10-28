de31b10dc544269aa7b7aacd1c0402f55f1e791e
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-10-28 10:49:31 +0300

    completed the tests

--- server/app.py
+++ server/app.py
@@ -1,26 +1,119 @@
 #!/usr/bin/env python3
-
 from flask import request, session
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
-
 from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        try:
+            data = request.get_json()
+            
+            if 'username' not in data:
+                return {'errors': ['Username is required']}, 422
+                
+            if 'password' not in data:
+                return {'errors': ['Password is required']}, 422
+                
+            new_user = User(
+                username=data['username'],
+                image_url=data.get('image_url'),
+                bio=data.get('bio')
+            )
+            new_user.password_hash = data['password']
+            
+            db.session.add(new_user)
+            db.session.commit()
+            
+            session['user_id'] = new_user.id
+            return new_user.to_dict(), 201  # Use SerializerMixin's to_dict()
+            
+        except (ValueError, IntegrityError) as e:
+            return {'errors': [str(e)]}, 422
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter_by(id=user_id).first()
+            return {
+                'id': user.id,
+                'username': user.username,
+                'image_url': user.image_url,
+                'bio': user.bio
+            }, 200
+        return {'error': 'Unauthorized'}, 401
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+        
+        user = User.query.filter_by(username=username).first()
+        
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return {
+                'id': user.id,
+                'username': user.username,
+                'image_url': user.image_url,
+                'bio': user.bio
+            }
+        return {'error': 'Unauthorized'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        if session.get('user_id'):
+            session.pop('user_id')
+            return {}, 204
+        return {'error': 'Unauthorized'}, 401
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        if not session.get('user_id'):
+            return {'error': 'Unauthorized'}, 401
+            
+        recipes = Recipe.query.all()
+        return [{
+            'id': recipe.id,
+            'title': recipe.title,
+            'instructions': recipe.instructions,
+            'minutes_to_complete': recipe.minutes_to_complete,
+            'user': {
+                'id': recipe.user.id,
+                'username': recipe.user.username
+            }
+        } for recipe in recipes], 200
+
+    def post(self):
+        if not session.get('user_id'):
+            return {'error': 'Unauthorized'}, 401
+            
+        try:
+            data = request.get_json()
+            new_recipe = Recipe(
+                title=data['title'],
+                instructions=data['instructions'],
+                minutes_to_complete=data['minutes_to_complete'],
+                user_id=session['user_id']
+            )
+            db.session.add(new_recipe)
+            db.session.commit()
+            
+            return {
+                'id': new_recipe.id,
+                'title': new_recipe.title,
+                'instructions': new_recipe.instructions,
+                'minutes_to_complete': new_recipe.minutes_to_complete,
+                'user': {
+                    'id': new_recipe.user.id,
+                    'username': new_recipe.user.username
+                }
+            }, 201
+        except ValueError as e:
+            return {'errors': [str(e)]}, 422
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
@@ -28,6 +121,5 @@ api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(Logout, '/logout', endpoint='logout')
 api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
 
-
-if __name__ == '__main__':
+if __name__ == '__main__':  
     app.run(port=5555, debug=True)
